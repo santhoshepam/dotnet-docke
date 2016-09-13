f9c6ec9f91b4e11d7826ae6d4729ccdfeaaad460
Michael Simons (VISUAL STUDIO) <msimons@microsoft.com>
2016-09-13 14:26:56 -0500

    Added CI for WindowsServerCore images

--- netci.groovy
+++ netci.groovy
@@ -2,16 +2,27 @@ import jobs.generation.Utilities
 
 def project = GithubProject
 def branch = GithubBranchName
-def isPr = true
-def containerOs = 'Debian'
-def newJobName = Utilities.getFullJobName(project, containerOs, isPr)
+def isPR = true
+def platformList = ['Ubuntu16.04:Debian', 'Windows_2016:WindowsServerCore']
 
-def newJob = job(newJobName) {
-    steps {
-        shell("./build-and-test.sh")
-    }
+platformList.each { platform ->
+	def(hostOS, containerOS) = platform.tokenize(':')
+	def newJobName = Utilities.getFullJobName(project, containerOS, isPR)
+
+	def newJob = job(newJobName) {
+		steps {
+			if (hostOS == 'Windows_2016')
+			{
+				batchFile("powershell -NoProfile -Command .\\build-and-test.ps1")
+			}
+			else
+			{
+				shell("./build-and-test.sh")
+			}
+		}
+	}
+
+	Utilities.setMachineAffinity(newJob, hostOS, 'latest-or-auto-docker')
+	Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+	Utilities.addGithubPRTriggerForBranch(newJob, branch, containerOS)
 }
-
-Utilities.setMachineAffinity(newJob, 'Ubuntu16.04', 'latest-or-auto-docker')
-Utilities.standardJobSetup(newJob, project, isPr, "*/${branch}")
-Utilities.addGithubPRTriggerForBranch(newJob, branch, containerOs)
