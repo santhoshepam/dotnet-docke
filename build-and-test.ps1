b9d133b3f1fcb56ff0846869881bcc5caa1e0706
Nate Amundson <naamunds@microsoft.com>
2016-09-22 11:43:37 -0700

    Add no-cache option to docker builds

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -13,7 +13,7 @@ pushd $PSScriptRoot
 Get-ChildItem -Recurse -Filter Dockerfile | where DirectoryName -like "*\$OS*" | sort DirectoryName | foreach {
     $tag = "$($dockerRepo):" + $_.DirectoryName.Replace($PSScriptRoot, '').TrimStart('\').Replace('\', '-') -replace "${OS}$", "${OS}-sdk"
     Write-Host "----- Building $tag -----"
-    docker build -t $tag $_.DirectoryName
+    docker build --no-cache -t $tag $_.DirectoryName
     if (-NOT $?) {
         throw "Failed building $tag"
     }
