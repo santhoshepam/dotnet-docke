f2aaca1a2884db0f808bccc51c07461051542c9f
Michael Simons <msimons@microsoft.com>
2016-10-27 14:36:04 -0500

    Update rel-1.0.0 SDK Dockerfile to build 1.0.0-preview3-003892 (#89)
    * Update debian Dockerfile to 1.0.0-preview3-003892
    
    * Update CI tests for intermediate CLI state regarding '3' verbs

--- test/create-run-publish-app.sh
+++ test/create-run-publish-app.sh
@@ -8,19 +8,30 @@ cd $1
 
 echo "Testing framework-dependent deployment"
 dotnet new
-dotnet restore
-dotnet run
-dotnet publish -o publish/framework-dependent
 
-echo "Testing self-contained deployment"
 if [ "rel-1.0.0-preview2.1" == "${2}" ]; then
-    cp /test/NuGet.Config .
+    dotnet restore
+    dotnet run
+    dotnet publish -o publish/framework-dependent
+else
+    dotnet restore3
+    dotnet run3
+    dotnet publish3 -o publish/framework-dependent
 fi
 
-runtimes_section="  },\n  \"runtimes\": {\n    \"debian.8-x64\": {}\n  }"
-sed -i '/"type": "platform"/d' ./project.json
-sed -i "s/^  }$/${runtimes_section}/" ./project.json
 
-dotnet restore
-dotnet run
-dotnet publish -o publish/self-contained
+echo "Testing self-contained deployment"
+
+if [ "rel-1.0.0-preview2.1" == "${2}" ]; then
+    cp /test/NuGet.Config .
+
+    runtimes_section="  },\n  \"runtimes\": {\n    \"debian.8-x64\": {}\n  }"
+    sed -i '/"type": "platform"/d' ./project.json
+    sed -i "s/^  }$/${runtimes_section}/" ./project.json
+
+    dotnet restore
+    dotnet run
+    dotnet publish -o publish/self-contained
+else
+    dotnet publish3 -r debian.8-x64 -o publish/self-contained
+fi
